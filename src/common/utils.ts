0fad4d1e37663297b4ef0d98413c22bf3dbf1c5b
Nikolai Perevozchikov <trueinsider@gmail.com>
2018-09-10 10:02:23 +0300

    Make binary representation of numbers match that of Go's VM (#1)
    * make binary representation of numbers match that of Go's VM
    
    * add test
    
    * Update format

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,20 +1,39 @@
-import * as ByteBuffer from 'bytebuffer';
 import * as Long from 'long';
 
-/**
- * TODO: check if the same as in Go
- */
+// tslint:disable:no-bitwise
+
 export function bigIntFromBytes(bytes: Buffer): Long {
-  const buffer = ByteBuffer.wrap(bytes, undefined, true);
-  return buffer.readInt64();
+  let data = Array.from(bytes.subarray(0));
+  const b = data[data.length - 1];
+
+  if (b >> 7 === 1) {
+    data = data.concat(Array(8 - data.length).fill(255));
+  }
+  return Long.fromBytesLE(data);
 }
 
-/**
- * TODO: check if the same as in Go
- */
 export function bigIntToBytes(value: Long) {
-  const buffer = new ByteBuffer(undefined, true);
-  buffer.writeUint64(value);
-  buffer.flip();
-  return new Buffer(buffer.toBuffer());
+  let data = value.toBytesLE();
+  const negData = value.neg().toBytesLE();
+  let stop;
+  if (value.isNegative()) {
+    stop = 255;
+  } else {
+    stop = 0;
+  }
+  let b = stop;
+  let pos = 0;
+  for (let i = data.length - 1; i >= 0; i--) {
+    if (data[i] !== stop) {
+      b = value.isNegative() ? negData[i] : data[i];
+      pos = i + 1;
+      break;
+    }
+  }
+  data = data.slice(0, pos);
+
+  if (b >> 7 === 1) {
+    data.push(value.isNegative() ? 255 : 0);
+  }
+  return new Buffer(data);
 }
