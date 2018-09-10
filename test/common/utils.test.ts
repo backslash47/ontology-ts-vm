a3015d2d7d8c3f53b72a1a135939838fa04f47ce
Matus Zamborsky <zamborsky@gmail.com>
2018-09-10 09:01:23 +0200

    Update utils.test.ts

--- test/common/utils.test.ts
+++ test/common/utils.test.ts
@@ -1,6 +1,6 @@
 import * as Long from 'long';
 
-import { bigIntFromBytes, bigIntToBytes } from '../../src/common/utils';
+import { bigIntFromBytes, bigIntToBytes } from '../src/common/utils';
 
 describe('Long encoding test', () => {
   test('Long encoding', async () => {
@@ -20,21 +20,13 @@ describe('Long encoding test', () => {
       new Buffer([171, 170, 170, 170, 170, 170, 171, 127])
     );
 
-    expect(bigIntToBytes(Long.fromString('-8380656'))).toEqual(
-      new Buffer([16, 31, 128])
-    );
+    expect(bigIntToBytes(Long.fromString('-8380656'))).toEqual(new Buffer([16, 31, 128]));
 
-    expect(bigIntToBytes(Long.fromString('8380656'))).toEqual(
-      new Buffer([240, 224, 127])
-    );
+    expect(bigIntToBytes(Long.fromString('8380656'))).toEqual(new Buffer([240, 224, 127]));
 
-    expect(bigIntToBytes(Long.fromString('-8446192'))).toEqual(
-      new Buffer([16, 31, 127, 255])
-    );
+    expect(bigIntToBytes(Long.fromString('-8446192'))).toEqual(new Buffer([16, 31, 127, 255]));
 
-    expect(bigIntToBytes(Long.fromString('8446192'))).toEqual(
-      new Buffer([240, 224, 128, 0])
-    );
+    expect(bigIntToBytes(Long.fromString('8446192'))).toEqual(new Buffer([240, 224, 128, 0]));
 
     expect(bigIntToBytes(Long.fromString('-0')).length).toBe(0);
 
@@ -42,40 +34,26 @@ describe('Long encoding test', () => {
   });
 
   test('Long decoding', async () => {
-    expect(bigIntFromBytes(
-      new Buffer([171, 170, 170, 170, 170, 170, 171, 128])
-    ).toString()).toBe('-9175052165852779861');
+    expect(bigIntFromBytes(new Buffer([171, 170, 170, 170, 170, 170, 171, 128])).toString()).toBe(
+      '-9175052165852779861'
+    );
 
-    expect(bigIntFromBytes(
-      new Buffer([85, 85, 85, 85, 85, 85, 84, 127])
-    ).toString()).toBe('9175052165852779861');
+    expect(bigIntFromBytes(new Buffer([85, 85, 85, 85, 85, 85, 84, 127])).toString()).toBe('9175052165852779861');
 
-    expect(bigIntFromBytes(
-      new Buffer([85, 85, 85, 85, 85, 85, 84, 128])
-    ).toString()).toBe('-9199634313818843819');
+    expect(bigIntFromBytes(new Buffer([85, 85, 85, 85, 85, 85, 84, 128])).toString()).toBe('-9199634313818843819');
 
-    expect(bigIntFromBytes(
-      new Buffer([171, 170, 170, 170, 170, 170, 171, 127])
-    ).toString()).toBe('9199634313818843819');
+    expect(bigIntFromBytes(new Buffer([171, 170, 170, 170, 170, 170, 171, 127])).toString()).toBe(
+      '9199634313818843819'
+    );
 
-    expect(bigIntFromBytes(
-      new Buffer([16, 31, 128])
-    ).toString()).toBe('-8380656');
+    expect(bigIntFromBytes(new Buffer([16, 31, 128])).toString()).toBe('-8380656');
 
-    expect(bigIntFromBytes(
-      new Buffer([240, 224, 127])
-    ).toString()).toBe('8380656');
+    expect(bigIntFromBytes(new Buffer([240, 224, 127])).toString()).toBe('8380656');
 
-    expect(bigIntFromBytes(
-      new Buffer([16, 31, 127, 255])
-    ).toString()).toBe('-8446192');
+    expect(bigIntFromBytes(new Buffer([16, 31, 127, 255])).toString()).toBe('-8446192');
 
-    expect(bigIntFromBytes(
-      new Buffer([240, 224, 128, 0])
-    ).toString()).toBe('8446192');
+    expect(bigIntFromBytes(new Buffer([240, 224, 128, 0])).toString()).toBe('8446192');
 
-    expect(bigIntFromBytes(
-      new Buffer([])
-    ).toString()).toBe('0');
+    expect(bigIntFromBytes(new Buffer([])).toString()).toBe('0');
   });
 });
