a3332e0dc4aa8e27eab423a6f59517eaecc4a755
Matus Zamborsky <zamborsky@gmail.com>
2018-09-27 15:42:49 +0200

    added throw tests

---
+++ test/python/throw.test.ts
@@ -0,0 +1,26 @@
+import 'babel-polyfill';
+import { isByteArrayType } from '../../src/vm/types/byteArray';
+import { isIntegerType } from '../../src/vm/types/integer';
+import { deployAndInvoke, loadContract } from '../utils';
+
+describe('Throw test', () => {
+  test('test 1', async () => {
+    const contract = loadContract('./test/python/compiled/example/throwTest.avm');
+
+    const response = await deployAndInvoke(contract, 1);
+    expect(isIntegerType(response.result)).toBeTruthy();
+    expect(response.result.getBoolean()).toBeTruthy();
+
+    await expect(deployAndInvoke(contract, 4)).rejects.toBeTruthy();
+  });
+
+  test('test 2', async () => {
+    const contract = loadContract('./test/python/compiled/example/throwIfNotTest.avm');
+
+    const response = await deployAndInvoke(contract, true);
+    expect(isIntegerType(response.result)).toBeTruthy();
+    expect(response.result.getBoolean()).toBeTruthy();
+
+    await expect(deployAndInvoke(contract, false)).rejects.toBeTruthy();
+  });
+});
